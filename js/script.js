11b83e36539e8bc3667e7b1c3c8d01a6c3e14941
Pawan Kumar <krpawan0308@gmail.com>
2026-08-06 19:06:55 +0530

    Making Buttons be visible in Display Area

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log("JavaScript Connected Successfully!");
+const display = document.getElementById("display");
+
+console.log(display); /*here, consule is used to verify that given selector is selecting right element  */
+
+const buttons = document.querySelectorAll(".btn");/*Select all buttons and make nodelist in which these buttons will be stored */
+console.log(buttons);
+
+const firstButton = document.querySelector(".btn");/*Select particular class in which JS will work */
+
+firstButton.addEventListener("click", function () {
+    console.log("Button Clicked!");
+}); /*EventListener is used to see what events is occur in browser and set function which will run after event execution */ 
+
+buttons.forEach(function(button) { /*this will work when page will load in browser*/
+    button.addEventListener("click", function() { /*Observe Clicked Button*/
+        display.textContent = button.textContent;/*It means show button-text in the display-text area*/
+    });
+});
